eb7169a14e2d68eaae04516f2de0d7223667256d
Fernando Ortega <feortegas@gmail.com>
2021-12-10 00:19:13 -0600

    finilized prompt questions

--- index.js
+++ index.js
@@ -8,59 +8,92 @@ const promptUser = () => {
         // Project Title
         {
             type: 'input',
-            name: 'name',
-            message: `What's the Project Title? `
+            name: 'title',
+            message: `What's the Project Title? `,
+            validate: titleInput => {
+                if (titleInput) {
+                  return true;
+                } else {
+                  console.log('You need to enter a project title!');
+                  return false;
+                }
+              }
         },
         // Project Description
         {
             type: 'input',
-            name: 'name',
-            message: `What's the Project Description? `
+            name: 'description',
+            message: `What's the Project Description? `,
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                  return true;
+                } else {
+                  console.log('You need to enter a project description!');
+                  return false;
+                }
+              }
         },
         // Project Installation
         {
             type: 'input',
-            name: 'name',
+            name: 'installation',
             message: 'What are the Project Installation steps? '
         },
         // Project Usage
         {
             type: 'input',
-            name: 'name',
+            name: 'usage',
             message: `What's the Project Usage? `
         },
         // Project License
         {
-            type: 'input',
-            name: 'name',
-            message: `What's the Project License? `
+            type: 'list',
+            name: 'license',
+            message: `What's the Project License? `,
+            choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozila Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']
         },
         // Project Contribution Guidelines
         {
             type: 'input',
-            name: 'name',
+            name: 'contributing',
             message: `What's the Project Contribution Guidelines? `
         },
         // Project Test Instructions
         {
             type: 'input',
-            name: 'name',
+            name: 'tests',
             message: `What's the Project Test Instructions? `
         },
         // Project - Github username
         {
             type: 'input',
-            name: 'name',
-            message: `What's your GitHub UserName? `
+            name: 'github',
+            message: `What's your GitHub UserName? `,
+            validate: githubInput => {
+                if (githubInput) {
+                  return true;
+                } else {
+                  console.log('You need to enter your GitHub username!');
+                  return false;
+                }
+              }
         },
         // Project - e-mail address
         {
             type: 'input',
-            name: 'name',
-            message: `What's your e-mail address? `
+            name: 'email',
+            message: `What's your e-mail address? `,
+            validate: emailInput => {
+                if (emailInput) {
+                  return true;
+                } else {
+                  console.log('You need to enter your e-mail address!');
+                  return false;
+                }
+              }
         }
     ])
-}
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
