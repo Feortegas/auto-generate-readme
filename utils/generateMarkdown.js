3c3b9cdc5792dba7a03677ead058604c3d206070
Fernando Ortega <feortegas@gmail.com>
2021-12-12 15:11:55 -0600

    add license badge handling

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,50 @@
+const { makeBadge, ValidationError } = require('badge-maker');
+const globalVariables = require('./licenses');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const format = {
+    label: 'License',
+    message: `${license}`,
+    color: 'orange'
+  }
+  const svg = makeBadge(format);
+  return svg;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch (license) {
+    case 'GNU AGPLv3':
+      return globalVariables.GNUAGPLv3;
+    case 'GNU GPLv3':
+      return globalVariables.GNUGPLv3;
+    case 'GNU LGPLv3':
+      return globalVariables.GNULGPLv3;
+    case 'Mozila Public License 2.0':
+      return globalVariables.MozilaPL2;
+    case 'Apache License 2.0':
+      return globalVariables.Apache;
+    case 'MIT License':
+      return globalVariables.MIT;
+    case 'Boost Software License 1.0':
+      return globalVariables.Boost;
+    case 'The Unlicense':
+      return globalVariables.theUnlicense;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data.license)}
 
   ## Description
   ${data.description}
@@ -24,7 +56,7 @@ function generateMarkdown(data) {
   ${data.usage}
 
   ## License
-  ${data.license}
+  ${renderLicenseSection(data.license)}
 
   ## Contributing
   ${data.contributing}
---
+++ utils/licenses.js
@@ -0,0 +1,141 @@
+const GNUAGPLv3 = `
+#### Permissions
+    - Commercial use
+    - Distribution
+    - Modification
+    - Patent use
+    - Private use
+
+ #### Conditions
+    - Disclose source
+    - License and copyright notice
+    - Network use is distribution
+    - Same license
+    - State changes
+
+ #### Limitations
+    - Liability
+    - Warranty`
+
+const GNUGPLv3 = `
+#### Permissions
+    - Commercial use
+    - Distribution
+    - Modification
+    - Patent use
+    - Private use
+
+#### Conditions
+    - Disclose source
+    - License and copyright notice
+    - Same license 
+    - State changes
+
+#### Limitations
+    - Liability
+    - Warranty`
+
+const GNULGPLv3 = `
+#### Permissions
+    - Commercial use
+    - Distribution
+    - Modification
+    - Patent use
+    - Private use
+
+#### Conditions
+    - Disclose source
+    - License and copyright notice
+    - Same license (library)
+    - State changes
+
+#### Limitations
+    - Liability
+    - Warranty`
+
+const MozilaPL2 = `
+#### Permissions
+    - Commercial use
+    - Distribution
+    - Modification
+    - Patent use
+    - Private use
+
+#### Conditions
+    - Disclose source
+    - License and copyright notice
+    - Same license (file)
+
+#### Limitations
+    - Liability
+    - Trademark use
+    - Warranty`
+
+const Apache = `
+#### Permissions
+    - Commercial use
+    - Distribution
+    - Modification
+    - Patent use
+    - Private use
+
+#### Conditions
+    - License and copyright notice
+    - State changes
+
+#### Limitations
+    - Liability
+    - Trademark use
+    - Warranty`
+
+const MIT = `
+#### Permissions
+    - Commercial use
+    - Distribution
+    - Modification
+    - Private use
+
+#### Conditions
+    - License and copyright notice
+
+#### Limitations
+    - Liability
+    - Warranty`
+
+const Boost = `
+#### Permissions
+    - Commercial use
+    - Distribution
+    - Modification
+    - Private use
+
+#### Conditions
+    - License and copyright notice for source
+
+#### Limitations
+    - Liability
+    - Warranty`
+
+const theUnlicense = `
+
+#### Permissions
+    - Commercial use
+    - Distribution
+    - Modification
+    - Private use
+
+#### Conditions
+    - None
+
+##### Limitations
+    - Liability
+    - Warranty`
+
+exports.GNUAGPLv3 = GNUAGPLv3;
+exports.GNUGPLv3 = GNUGPLv3;
+exports.GNULGPLv3 = GNULGPLv3;
+exports.MozilaPL2 = MozilaPL2;
+exports.Apache = Apache;
+exports.MIT = MIT;
+exports.Boost = Boost;
+exports.theUnlicense = theUnlicense;
